959af1d686e2952717a85a579dca72968dc0960d
Vijayeshjeevan Matam <vijayeshjeevan.cs19@bmsce.ac.in>
2021-06-01 16:52:43 +0530

    Code correction

--- 9_functions1.c
+++ 9_functions1.c
@@ -1,21 +1,20 @@
-//Given that the volume of tromboloid is twice the length divided by thrice the breadth
 #include<stdio.h>
 
-float tromboloid_vol(int, int);
+float cone_vol(int, int);
 
 int main()
 {
-    int length, breadth;
-    printf("Enter the length of the tromboloid:\t");
-    scanf("%d", &length);
-    printf("Enter the breadth of the tromboloid:\t");
-    scanf("%d", &breadth);
-    float volume = tromboloid_vol(length, breadth);
-    printf("\nThe volume of the tromboloid is:\t%f", volume);
+    int radius, height;
+    printf("Enter the radius of the cone:\t");
+    scanf("%d", &radius);
+    printf("Enter the height of the cone:\t");
+    scanf("%d", &height);
+    float volume = cone_vol(radius, height);
+    printf("\nThe volume of the cone is:\t%f", volume);
     return 0;
 }
 
-float tromboloid_vol(int l, int b)
+float cone_vol(int l, int b)
 {
     float vol = (2*l)/(3*b);
     printf("%f checking", vol);
