f7302e84db64c7892861f9282d2a38613346a2f4
sumedhdkme19 <72097989+sumedhdkme19@users.noreply.github.com>
2021-06-01 15:07:41 +0530

    Proper commit

--- 1_hello_world.c
+++ 1_hello_world.c
@@ -1,7 +1,7 @@
-#include<stdio.h>//Header file. Header files contain the function prototypes for basic io operations.
+#include<stdio.h>//Header file. This header file contains the function prototypes of standard input/output functions such as printf and scanf.
 
 int main()
 {
     printf("Hello world!");
-    return 0;//0 represents the status code.
+    return 0;  //0 represents the exit status.
 }
