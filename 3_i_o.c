f60ee92f2e190f3f255051c75d18160c21ee5272
sumedhdkme19 <72097989+sumedhdkme19@users.noreply.github.com>
2021-06-01 15:54:26 +0530

    Proper commit

--- 3_i_o.c
+++ 3_i_o.c
@@ -3,11 +3,11 @@
  int main()
  {
      int fav_no;
-     printf("Enter your favourite number:\t");
-     scanf("%d", &fav_no);
+     printf("Enter your favourite number: ");
+     scanf("%d",&fav_no);//
      char alpha;
      printf("Enter a random character:\t");
-     scanf("\n%c", &alpha);
+     scanf("%c",&alpha);
 
      printf("Your favourite number is: %d\nYour entered Character is: %d", fav_no, alpha);
  }
